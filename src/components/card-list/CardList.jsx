80318c59dcaa0c3ddb19ac65ef315227cd4aab7b
luqi <lq.xie.work@gmail.com>
2025-01-06 00:13:59 -0800

    move sortByLikes and Reset to App.jsx and refactor CardList

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -1,47 +1,22 @@
 import PropTypes from 'prop-types';
 import './CardList.css';
 import Card from '../card/Card';
-import { useState, useEffect } from 'react';
 
 const CardList = ({ cards, onDeleteCard, onRateCard }) => {
-    const [isSortByRate, setIsSortByRate] = useState(false);
-    const [sortedCards, setSortedCards] = useState(cards);
-
-    useEffect(() => {
-        if (isSortByRate) {
-            const sorted = [...cards].sort((a, b) => b.likes_count - a.likes_count);
-            setSortedCards(sorted);
-        } else {
-            setSortedCards(cards);
-        }
-    }, [isSortByRate, cards]); 
-
-    const handleSortByRate = () => {
-        setIsSortByRate(true); 
-    };
-
-    const handleResetSorting = () => {
-        setIsSortByRate(false);
-    };
-
-    return (
-        <div className="card-list">
-            <div className="sortcards">
-                <button onClick={handleSortByRate}>Sort by Likes</button>
-                <button onClick={handleResetSorting}>Reset</button>
-            </div>
-            {
-                sortedCards.map(({ card_id, message, likes_count }) => <Card
-                    key={card_id}
-                    card_id={card_id}
-                    message={message}
-                    likes_count={likes_count}
-                    onDeleteCard={onDeleteCard}
-                    onRateCard={onRateCard}
-                />)
-            }
-        </div>
-    );
+  return (
+    <div className="card-list">
+      {cards.map(({ card_id, message, likes_count }) => (
+        <Card
+          key={card_id}
+          card_id={card_id}
+          message={message}
+          likes_count={likes_count}
+          onDeleteCard={onDeleteCard}
+          onRateCard={onRateCard}
+        />
+      ))}
+    </div>
+  );
 };
 
 CardList.propTypes = {
