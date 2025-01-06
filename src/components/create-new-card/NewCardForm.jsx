dd8db35f60d43b540dc7fe5b4e24fcf254a2d17d
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-01-05 20:25:23 -0600

    error handling for card message exceeding character limit, lack of card message, and edge case

--- src/components/create-new-card/NewCardForm.jsx
+++ src/components/create-new-card/NewCardForm.jsx
@@ -4,16 +4,35 @@ import { useState } from 'react';
 
 const NewCardForm = ({ onAddCard }) => {
     const [formCard, setFormCard] = useState('');
+    const [error, setError] = useState('');
 
     const handleCardChange = (event) => {
             setFormCard(event.target.value);
+            if (event.target.value.length <= 40 && (event.target.value.trim() !== '')) {
+                setError('');
+            };
         };
 
     const handleCardSubmit = (event) => {
         event.preventDefault();
+
+        if (!formCard) {
+            setError('Required');
+            return;
+        }
+        if (formCard.length > 40) {
+            setError('Card message must not exceed 40 characters');
+            return;
+        }
+        if (!formCard.trim()) {
+            setError('Invalid message');
+            return;
+        }
+        
         const newCard = {
             message: formCard,
         };
+        setError('');
         onAddCard(newCard);
         setFormCard('');
     };
@@ -28,7 +47,9 @@ const NewCardForm = ({ onAddCard }) => {
                 value={formCard}
                 onChange={handleCardChange}
                 />
+            <label htmlFor="card-error">{error}</label>
             <label htmlFor="card-preview">{`Preview: ${formCard ? formCard : ''}`}</label>
+            <label htmlFor="card-character-count-preview">{`${formCard ? formCard.length : '0'} / 40`}</label>
             <button type='submit'>Submit</button>
         </form>
     );
